81ead0fe1c7066cba27b6e9fff2db46fbb7091a2
shadrack5 <shadrackkibet100@gmail.com>
2023-10-03 14:34:28 +0300

    routing functions

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
@@ -11,7 +12,6 @@ ns = Namespace("/")
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 
 migrate = Migrate(app, db)
 
@@ -59,3 +59,93 @@ hero_power_input_model = api.model(
     },
 )
 
+
+@api.errorhandler(ObjectNotFoundException)
+def handle_no_result_exception(error):
+    """Return a custom not found error message and 404 status code"""
+    return {"error": error.message}, 404
+
+
+@api.errorhandler(ValueError)
+def handle_no_result_exception(error):
+    """Return a custom not found error message and 404 status code"""
+    return {"errors": error.args}, 400
+
+
+@ns.route("/heroes")
+class HeroResource(Resource):
+    @ns.marshal_list_with(hero_model)
+    def get(self):
+        return Hero.query.all()
+
+
+@ns.route("/heroes/<int:id>")
+class HeroByIdResource(Resource):
+    @ns.marshal_with(single_hero_model)
+    def get(self, id):
+        hero = Hero.query.filter_by(id=id).first()
+        if not hero:
+            raise ObjectNotFoundException("Hero not found")
+        else:
+            return hero
+
+
+@ns.route("/powers")
+class PowerResource(Resource):
+    @ns.marshal_list_with(power_model)
+    def get(self):
+        return Power.query.all()
+
+
+@ns.route("/powers/<int:id>")
+class PowerByIdResource(Resource):
+    @ns.marshal_with(power_model)
+    def get(self, id):
+        power = Power.query.filter_by(id=id).first()
+        if not power:
+            raise ObjectNotFoundException("Power not found")
+        else:
+            return power
+
+    @ns.expect(power_input_model)
+    @ns.marshal_with(power_model)
+    def patch(self, id):
+        power = Power.query.filter_by(id=id).first()
+        if not power:
+            raise ObjectNotFoundException("Power not found")
+        else:
+            try:
+                for attr in ns.payload:
+                    setattr(power, attr, ns.payload[attr])
+                db.session.add(power)
+                db.session.commit()
+                return power
+            except ValueError as e:
+                raise e
+
+
+@ns.route("/hero_powers")
+class HeroPowerResource(Resource):
+    @ns.marshal_list_with(hero_power_model)
+    def get(self):
+        return HeroPower.query.all()
+
+    @ns.expect(hero_power_input_model)
+    @ns.marshal_with(single_hero_model)
+    def post(self):
+        try:
+            hero_power = HeroPower(
+                strength=ns.payload["strength"],
+                power_id=ns.payload["power_id"],
+                hero_id=ns.payload["hero_id"],
+            )
+            db.session.add(hero_power)
+            db.session.commit()
+            hero = Hero.query.filter_by(id=ns.payload["hero_id"]).first()
+            return hero, 201
+        except ValueError as e:
+            raise e
+
+
+if __name__ == "__main__":
+    app.run(port=5555)
